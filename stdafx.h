de6d8cdc4593e0a3b2a345e23b4b364b6319d1f6
MARGINALITY\hiyohiyo <hiyohiyo@crystalmark.info>
2019-10-15 22:54:40 +0900

    CrystalDiskMark 7.0.0 RC2 - 正式版に向けた修正

--- stdafx.h
+++ stdafx.h
@@ -110,7 +110,7 @@
 /// Beta
 // #define PRODUCT_EDITION			_T("-Marginality-")
 
-#define PRODUCT_RELEASE			_T("2019/10/13")
+#define PRODUCT_RELEASE			_T("2019/10/15")
 #define PRODUCT_COPY_YEAR		_T("2007-2019")
 #define PRODUCT_COPYRIGHT		_T("© hiyohiyo 2007-2019")
 #define PRODUCT_LICENSE			_T("The MIT License")
